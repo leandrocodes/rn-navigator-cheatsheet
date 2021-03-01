dcb77a998a1a603d7c818d899a9b826b60355cf1
leandrocodes <leandrovianacodes@gmail.com>
2021-03-01 17:45:18 -0400

    hello app

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+import CentralText from './components/CentralText'
+
+export default () => (
+	<View style={{ flex: 1 }}>
+		<CentralText>Hello from App!</CentralText>
+	</View>
+)
